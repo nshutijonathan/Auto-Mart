2c157e1a604580c6b98b3c0492e51651d6a0122f
jonathan nshuti <nshutijonathan130@gmail.com>
2019-05-14 10:21:04 +0200

    [(ft1-user-purchase-order-165964841]added landind.css.

--- js/auth.js
+++ js/auth.js
@@ -15,3 +15,12 @@ function toggleLogin(){
     document.getElementById("signup-form").style.display="none";
     document.getElementById("login-form").style.display="block";
 }
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
