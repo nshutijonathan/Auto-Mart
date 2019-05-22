1afacc28e5625f5e312a671af06f277b46dd7eb8
jonathan nshuti <nshutijonathan130@gmail.com>
2019-05-22 10:49:20 -0700

    [ch-server-setup-166175515]configured eslint.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
-import express from "express";
-const app=express();
+import express from 'express';
+import bodyParser from 'body-parser';
+// set  up the express app
+const app = express();
 
-app.get('/',(req,res)=>{
-	return res.status(200).send({
-		status:200;
-		message:"Welcome to Auto mart"
-	})
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// When a random route is inputed
+app.get('*', (req, res) => {
+  return res.status(200).send({
+    status: 200,
+    message: 'Welcome to Auto mart'
+  });
 });
 
-const port=process.env.PORT ||3000;
-app.listen(port,()=>(console.log(`Listening on port ${port}`)));
+const port = process.env.PORT || 3000;
+app.listen(port, () => (console.log(`Listening on port ${port}`)));
+export default app;
