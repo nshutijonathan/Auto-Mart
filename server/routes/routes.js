769b8be2739a9e55088743713726bfaaae12eaa7
jonathan nshuti <nshutijonathan130@gmail.com>
2019-05-24 09:34:37 -0700

    [ft2-seller-create-sale-ad-166232307]renamed cars models.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -8,4 +8,7 @@ router.post('/api/v1/auth/signin', Users.signinuser);
 router.get('/api/v1/users/:id', Users.getoneuser);
 router.get('/api/v1/users', Users.getallusers);
 router.delete('/api/v1/users/:id', Users.deleteuser);
+
+// cars advert routes
+router.get('/api/v1/cars', Cars.getallcarsad);
 export default router;
