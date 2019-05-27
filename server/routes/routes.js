8653f6d35567a1ee5c9074030a6b717ce2e0d2d8
jonathan nshuti <nshutijonathan130@gmail.com>
2019-05-27 14:44:44 -0700

    [ft2-seller-update-ad-as-sold-166268073]implemented car's updaated status logic.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -19,5 +19,5 @@ router.post('/api/v1/car', Cars.createadvert);
 router.get('/api/v1/orders', Orders.getallorders);
 router.post('/api/v1/order', Orders.createorder);
 router.put('/api/v1/order/:id/price', Orders.updateorder);
-router.put('/api/v1/car/:id/status',Orders.)
+router.put('/api/v1/car/:id/status', Orders.updatestatus);
 export default router;
--- server/validations/purchaseorders.js
+++ server/validations/purchaseorders.js
@@ -9,5 +9,12 @@ class Ordersvalidations {
       throw Error('this status field must be pending,accepted,or rejected ');
     }
   }
+
+
+  static statusupdate(req, res) {
+  	if ((req.body.status !== 'sold' && req.body.status !== 'available')) {
+      throw Error('this status field must be sold or available ');
+    }
+  }
 }
 export default Ordersvalidations;
