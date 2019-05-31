94e232aaf9aea283e72ba91bcd7ccda9b31edb36
jonathan nshuti <nshutijonathan130@gmail.com>
2019-05-31 11:04:28 -0700

    [ft2-ci-testing-166373040]added coverage script.

--- server/tests/users.js
+++ server/tests/users.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-Http';
 import server from '../server';
+import UserData from '../models/users';
 
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaXNfYWRtaW4iOiJ0cnVlIiwiaWF0IjoxNTU5MzE0Nzg3fQ.wM5wguPZqOuTujyal5oYYBYy4ONDsuwIy6HH9ODvnEA';
 chai.use(chaiHttp);
@@ -39,5 +40,25 @@ describe('get one single user', () => {
       res.body.data.should.be.an('object');
       done();
     });
+  });
+  it('should not return single user', (done) => {
+    chai.request(server).get('/api/v1/users/7').set('x-auth-token', token)
+      .end((err, res) => {
+        console.log(res.body);
+        res.body.should.be.an('object');
+        res.body.should.have.property('status').eql(404);
+        res.body.should.have.property('message').eql('User with id 7 not found');
+        done();
+      });
+  });
+  it('should not return single user', (done) => {
+    chai.request(server).get('/api/v1/users/7')
+      .end((err, res) => {
+        console.log(res.body);
+        res.body.should.be.an('object');
+        res.body.should.have.property('status').eql(402);
+        res.body.should.have.property('message').eql('Access denied. no token provided');
+        done();
+      });
   });
 });
